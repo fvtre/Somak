8ffbcf309fc2acf8ddfd06a44e8319d5477f2940
fvtre <ad.lecaros@duocuc.cl>
2024-11-02 23:26:02 -0300

    Mensaje que describe los cambios

--- IECA/settings.py
+++ IECA/settings.py
@@ -81,10 +81,11 @@ WSGI_APPLICATION = 'IECA.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost/postgres',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        
+    }
 }
 
 
